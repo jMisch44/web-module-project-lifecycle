b0af7180b9dfe4ca1f7fd18e22ef79bf767373fe
jMisch44 <jennimisch@gmail.com>
2021-08-31 15:01:39 -0400

    jsx created

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        src="https://images.unsplash.com/photo-1608085021978-6981f6993f9c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=784&q=80"
+        alt="placeholder image"
+      />
+      <h1>login</h1>
+      <p>name</p>
+      <p>bio</p>
     </div>
   );
 }
