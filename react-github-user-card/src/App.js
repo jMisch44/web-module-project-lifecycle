3bd9e5bcb47518ef029f4973a2105607a5e5aeba
jMisch44 <jennimisch@gmail.com>
2021-08-31 19:24:07 -0400

    working on search bar

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
 class App extends React.Component {
   state = {
-    gitUser: "",
+    gitUser: "jMisch44",
     gitUserData: {},
     gitUserFollowers: [],
   };
 
   componentDidMount() {
     axios
-      .get(`https://api.github.com/users/jMisch44`)
+      .get(`https://api.github.com/users/${this.state.gitUser}`)
       .then((res) => {
         this.setState({
           ...this.state,
@@ -22,7 +22,7 @@ class App extends React.Component {
         console.error(err);
       });
     axios
-      .get(`https://api.github.com/users/wlongmire/followers`)
+      .get(`https://api.github.com/users/${this.state.gitUser}/followers`)
       .then((res) => {
         this.setState({
           ...this.state,
@@ -34,9 +34,55 @@ class App extends React.Component {
       });
   }
 
+  // componentDidUpdate() {
+
+  // }
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      gitUser: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${this.state.gitUser}`)
+      .then((res) => {
+        this.setState({
+          ...this.state,
+          gitUserData: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    axios
+      .get(`https://api.github.com/users/${this.state.gitUser}/followers`)
+      .then((res) => {
+        this.setState({
+          ...this.state,
+          gitUserFollowers: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   render() {
     return (
       <div className="App">
+        <header>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              // value={this.state.gitUser}
+              onChange={this.handleChange}
+            />
+            <button>Search for User</button>
+          </form>
+        </header>
         <div className="card-container">
           <div className="non-followers card">
             <img
@@ -51,10 +97,13 @@ class App extends React.Component {
             </div>
           </div>
           <h3>followers:</h3>
-
-          {this.state.gitUserFollowers.map((follower) => {
-            return <p className="followers">{follower.login}, </p>;
-          })}
+          {this.state.gitUserFollowers === [] ? (
+            this.state.gitUserFollowers.map((follower) => {
+              return <p className="followers">{follower.login}, </p>;
+            })
+          ) : (
+            <p>No followers</p>
+          )}
         </div>
       </div>
     );
